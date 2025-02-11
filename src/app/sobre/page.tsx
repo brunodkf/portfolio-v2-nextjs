d72d82185a1fc0648295cf94f3d28df84c9b568d
Bruno Furtado <bsouzacc@gmail.com>
2025-02-11 01:50:37 -0300

    update /pré estilização do footer

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Image from "next/image";
 
 export default function Sobre(){
     return (
         <main className="home container w-full h-svh flex items-center justify-center">
-            <Image src='/hex.svg' alt="Hexágono" className="md:w-[50%]" width={100} height={100}/>
 
             <div className="relative w-full flex items-center justify-center">
                     <div className={`absolute inset-0 text-branco flex flex-col items-center justify-center`}>
